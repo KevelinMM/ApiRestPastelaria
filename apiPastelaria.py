1685fb464e9033a48902ab987206d23603062c94
kevelinMM <kevelinmmattos@uniplaclages.edu.br>
2022-08-31 19:54:57 -0300

    aula 31/08

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -1,20 +1,9 @@
-from flask import Flask
-from flask_restful import Api
+from fastapi import FastAPI
 
-# import das classes com os endpoints
-from FuncionarioDAO import Funcionario
+import FuncionarioDAO
+import ClienteDAO
 
-# import das classes com os endpoints
-from ClienteDAO import Cliente
+app = FastAPI()
 
-app = Flask(__name__)
-api = Api(app)
-
-# mapeamento dos endpoints
-api.add_resource(Funcionario, "/funcionario/<int:id>", endpoint = 'funcionario')
-# mapeamento dos endpoints
-api.add_resource(Cliente, "/cliente/<int:id>", endpoint = 'cliente')
-
-if __name__ == "__main__":
-    """ Inicia a API Flask RESTful """
-    app.run(host='0.0.0.0', port=5000, debug=True)
+app.include_router(FuncionarioDAO.router)
+app.include_router(ClienteDAO.router)
